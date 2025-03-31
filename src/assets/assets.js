3d69afde07db7563627be1bed2681ca1f047add0
ds-arunkumar <arunkumar519@gmail.com>
2025-03-31 15:47:48 +0530

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,204 @@
+import bell_icon from "./bell.png";
+import home_icon from "./home.png";
+import like_icon from "./like.png";
+import loop_icon from "./loop.png";
+import mic_icon from "./mic.png";
+import next_icon from "./next.png";
+import play_icon from "./play.png";
+import pause_icon from "./pause.png";
+import plays_icon from "./plays.png";
+import prev_icon from "./prev.png";
+import search_icon from "./search.png";
+import shuffle_icon from "./shuffle.png";
+import speaker_icon from "./speaker.png";
+import stack_icon from "./stack.png";
+import zoom_icon from "./zoom.png";
+import plus_icon from "./plus.png";
+import arrow_icon from "./arrow.png";
+import mini_player_icon from "./mini-player.png";
+import queue_icon from "./queue.png";
+import volume_icon from "./volume.png";
+import arrow_right from "./right_arrow.png";
+import arrow_left from "./left_arrow.png";
+import spotify_logo from "./spotify_logo.png";
+import clock_icon from "./clock_icon.png";
+
+import thumb1 from "./img/song-thumb/thumb1.webp";
+import thumb2 from "./img/song-thumb/thumb2.png";
+import thumb3 from "./img/song-thumb/thumb3.png";
+import thumb4 from "./img/song-thumb/thumb4.jpeg";
+import thumb5 from "./img/song-thumb/thumb5.png";
+import thumb6 from "./img/song-thumb/thumb6.png";
+import thumb7 from "./img/song-thumb/thumb7.png";
+import thumb8 from "./img/song-thumb/thumb8.png";
+
+import img1 from "./img/img1.jpg";
+import img2 from "./img/img2.jpg";
+import img3 from "./img/img3.jpg";
+import img4 from "./img/img4.jpg";
+import img5 from "./img/img5.jpg";
+import img6 from "./img/img6.jpg";
+import img7 from "./img/img7.jpg";
+import img8 from "./img/img8.jpg";
+
+import song1 from "./songs/song1.mp3";
+import song2 from "./songs/song2.mp3";
+import song3 from "./songs/song3.mp3";
+import song4 from "./songs/song4.mp3";
+import song5 from "./songs/song5.mp3";
+import song6 from "./songs/song6.mp3";
+import song7 from "./songs/song7.mp3";
+import song8 from "./songs/song8.mp3";
+
+export const assets = {
+  bell_icon,
+  home_icon,
+  like_icon,
+  loop_icon,
+  mic_icon,
+  next_icon,
+  play_icon,
+  plays_icon,
+  prev_icon,
+  search_icon,
+  shuffle_icon,
+  speaker_icon,
+  stack_icon,
+  zoom_icon,
+  plus_icon,
+  arrow_icon,
+  mini_player_icon,
+  volume_icon,
+  queue_icon,
+  pause_icon,
+  arrow_left,
+  arrow_right,
+  spotify_logo,
+  clock_icon,
+};
+
+export const albumsData = [
+  {
+    id: 0,
+    name: "Munpani Kaadhal",
+    image: img5,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#D10000",
+  },
+  {
+    id: 1,
+    name: "10s Romance Tamil",
+    image: img1,
+    desc: "Best Love Songs of 2010s from kollywood",
+    bgColor: "#bcc45e",
+  },
+  {
+    id: 2,
+    name: "A.R. Rahman",
+    image: img2,
+    desc: "Hits of A.R. Rahman that will kill you",
+    bgColor: "#029ed6",
+  },
+  {
+    id: 3,
+    name: "Kollywood Chill",
+    image: img4,
+    desc: "Cool tracks from Kollywood music industry",
+    bgColor: "#44337a",
+  },
+  {
+    id: 4,
+    name: "Purely Kadhal",
+    image: img6,
+    desc: "All the mushy feels from top romantic hits",
+    bgColor: "#D10000",
+  },
+  {
+    id: 5,
+    name: "Romantic Anirudh",
+    image: img7,
+    desc: "Enjoy the Romantic side of Ani",
+    bgColor: "#744210",
+  },
+  {
+    id: 6,
+    name: "Hip Hop Tamizha",
+    image: img3,
+    desc: "Love and more love from Aadhi",
+    bgColor: "#F7567C",
+  },
+  {
+    id: 7,
+    name: "img8 Hits",
+    image: img8,
+    desc: "Love and more love from Aadhi",
+    bgColor: "#4E3822",
+  },
+];
+
+export const songsData = [
+  {
+    id: 0,
+    name: "Song Four",
+    image: thumb1,
+    file: song4,
+    desc: "Happy tunes from Dhanush",
+    duration: "4:44",
+  },
+  {
+    id: 1,
+    name: "Song Three",
+    image: thumb2,
+    file: song3,
+    desc: "Dive into anirudh sad zone",
+    duration: "4:22",
+  },
+  {
+    id: 2,
+    name: "Song Two",
+    image: thumb4,
+    file: song2,
+    desc: "Crazy Love from Dhanush",
+    duration: "5:04",
+  },
+  {
+    id: 3,
+    name: "Song One",
+    image: thumb5,
+    file: song1,
+    desc: "Mesmerizing hits A.R.R > anything",
+    duration: "5:10",
+  },
+  {
+    id: 4,
+    name: "Song Five",
+    image: thumb3,
+    file: song5,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "6:01",
+  },
+  {
+    id: 5,
+    name: "Song Six",
+    image: thumb7,
+    file: song6,
+    desc: "Dont ever feel down listen this",
+    duration: "3:59",
+  },
+  {
+    id: 6,
+    name: "Song Seven",
+    image: thumb6,
+    file: song7,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "3:59",
+  },
+  {
+    id: 7,
+    name: "Song Eight",
+    image: thumb8,
+    file: song8,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "4:35",
+  },
+];
