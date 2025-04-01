1917727485a4abb6525bbfda180ce80afd07804b
ds-arunkumar <arunkumar519@gmail.com>
2025-04-01 10:51:03 +0530

    commit two

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Navbar from './Navbar'
-import { albumsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
-import { songsData } from '../assets/assets'
 import SongItem from './SongItem'
+import { PlayerContext } from '../context/PlayerContext'
 
 const DisplayHome = () => {
+  const {songsData, albumsData} = useContext(PlayerContext);
   return (
     <>
     <Navbar/>
@@ -17,7 +17,7 @@ const DisplayHome = () => {
             key={index} 
             name={item.name} 
             desc={item.desc} 
-            id={item.id} 
+            id={item._id} 
             image={item.image}
           />))}
       </div>
@@ -25,7 +25,7 @@ const DisplayHome = () => {
     <div className='mb-4'>
       <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
       <div className='flex overflow-auto'>
-        {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+        {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image}/>))}
       </div>
     </div>
     </>
--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,5 +1,6 @@
+import axios from "axios";
 import { createContext, useEffect, useRef, useState } from "react";
-import { songsData } from "../assets/assets";
+
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const PlayerContext = createContext();
@@ -9,7 +10,10 @@ const PlayerContextProvider = (props)=>{
   const seekBg = useRef();
   const seekBar = useRef();
 
+  const url = 'http://localhost:4000';
 
+  const [songsData,setSongsData] = useState([]);
+  const [albumsData,setAlbumsData]= useState([]);
 
 
   const [track, setTrack] = useState(songsData[0])
@@ -37,34 +41,63 @@ const PlayerContextProvider = (props)=>{
   }
 
   const playWithId = async (id) => {
-    await setTrack(songsData[id]);
+    await songsData.map((item)=>{
+      if (id === item._id){
+        setTrack(item);
+      }
+    })
+
     await audioRef.current.play();
-    setPlayStatus(true)
+    setPlayStatus(true);
 
   }
 
   const previous = async () => {
-    if (track.id>0){
-      await setTrack(songsData[track.id-1]);
-      await audioRef.current.play();
-      setPlayStatus(true)
+    songsData.map(async(item,index)=>{
+      if(track._id === item._id && index > 0){
+        await setTrack(songsData[index-1]);
+        await audioRef.current.play()
+        setPlayStatus(true);
 
-    }
+      }
+
+    })
   }
 
   const next = async () => {
-    if (track.id < songsData.length-1){
-      await setTrack(songsData[track.id+1]);
-      await audioRef.current.play();
-      setPlayStatus(true)
+    songsData.map(async(item,index)=>{
+      if(track._id === item._id && index < songsData.length){
+        await setTrack(songsData[index+1]);
+        await audioRef.current.play()
+        setPlayStatus(true);
 
-    }
+      }
+
+    })
   }
 
   const seekSong = async (e)=>{
     audioRef.current.currentTime = ((e.nativeEvent.offsetX / seekBg.current.offsetWidth)*audioRef.current.duration)
 
 
+  }
+
+  const getSongsData = async () => {
+    try{
+      const response = await axios.get(`${url}/api/song/list`);
+      setSongsData(response.data.songs);
+      setTrack(response.data.songs[0])
+
+    } catch(error){}
+  }
+
+  const getAlbumsData = async () => {
+    try{
+      const response = await axios.get(`${url}/api/album/list`);
+      setAlbumsData(response.data.albums);
+    } catch (error){
+
+    }
   }
 
   useEffect(()=>{
@@ -87,6 +120,13 @@ const PlayerContextProvider = (props)=>{
 
   },[audioRef])
 
+  useEffect(()=>{
+    getSongsData();
+    getAlbumsData();
+
+
+  },[])
+
 
 
   const contextValue = {
@@ -99,7 +139,9 @@ const PlayerContextProvider = (props)=>{
     play,pause,
     playWithId,
     previous,next,
-    seekSong
+    seekSong,
+    songsData,
+    albumsData
 
   }
   return(
